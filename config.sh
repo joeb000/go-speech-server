18d0a2409dedcbc6caad630deac24c5b24b2ccd6
Joe Bernitt <j.b5286@gmail.com>
2019-11-14 01:06:09 -0500

    config updates

--- config.sh
+++ config.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
-
-curl https://github.com/mozilla/DeepSpeech/releases/download/v0.5.1/deepspeech-0.5.1-models.tar.gz
-
-tar xvfz deepspeech-0.5.1-models.tar.gz
-
+echo "Downloading data model files to config directory..."
+curl -o models.tar -L https://github.com/mozilla/DeepSpeech/releases/download/v0.5.1/deepspeech-0.5.1-models.tar.gz
+tar xvfz models.tar
 mv deepspeech-0.5.1-models/* config/.
 rm -rf deepspeech-0.5.1-model*
+rm -rf models.tar
+
+# build docker image - could take a couple min
+echo "Building docker image, this may take a couple minutes..."
+docker build -t test-server .
+
+echo "Running server in detached docker container on port 8080..."
+docker run -d -p 8080:8080 test-server
+
+echo "DONE"
