b2d1b139861d61ec6ab9c24ba657a507129b45e9
Joe Bernitt <j.b5286@gmail.com>
2019-11-14 00:51:58 -0500

    cofnfig

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -O https://github.com/mozilla/DeepSpeech/releases/download/v0.5.1/deepspeech-0.5.1-models.tar.gz
+curl https://github.com/mozilla/DeepSpeech/releases/download/v0.5.1/deepspeech-0.5.1-models.tar.gz
 
 tar xvfz deepspeech-0.5.1-models.tar.gz
 
